2a3531cd2aca3b66d803667c4769e16d0e33ed4c
ccseyhan <cemalcan.seyhan@deop.ca>
2022-12-17 20:39:42 -0300

    initial commit

---
+++ providers.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.36.0"
+    }
+  }
+    backend "remote" {
+
+    organization = "ccseyhan"
+
+    workspaces {
+
+      name = "terraform_cloud"
+
+    }
+
+   
+
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+}
